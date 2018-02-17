6762bc8c4d46f841528af7892b42d32dd272b58f
Krishna Pal Rajput <36352843+nagatoUzumaki@users.noreply.github.com>
2018-02-17 18:56:39 +0530

    Update Euler001.py

--- Euler001.py
+++ Euler001.py
@@ -5,16 +5,10 @@ for a in range (t):
     lst.append(n)
     sum = 0
 for n in lst:
-    for i in range (n):
-        if (i % 3 == 0) or (i % 5 == 0):
-            sum += i
-    print(sum)
-
-
-    n1 = 0, n2 = 0, n3 = 0;
-    n1 = n / 3;
-    n1 = 3 * n1 * (n1 + 1) / 2;
-n2 = n / 5 - 1;
-n2 = 5 * n2 * (n2 + 1) / 2;
-n3 = n / 15;
-n3 = 15 * n3 * (n3 + 1) / 2;
+    n1 = n // 3
+    n1 = 3 * n1 * (n1 + 1) // 2
+    n2 = n // 5
+    n2 = 5 * n2 * (n2 + 1) // 2
+    n3 = n // 15
+    n3 = 15 * n3 * (n3 + 1) // 2
+    print(n1 + n2 -n3)
